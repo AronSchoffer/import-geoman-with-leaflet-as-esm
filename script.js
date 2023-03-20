5f45f50ca7cee314f4335bf35924f988d4739e44
Aron Schöffer <aronschoffer@gmail.com>
2023-03-20 13:12:01 +0100

    lazy load geoman

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // This works:
-import './node_modules/leaflet/dist/leaflet.js';
+// import './node_modules/leaflet/dist/leaflet.js';
 // This doesn't:
-// import * as L from './node_modules/leaflet/dist/leaflet-src.esm.js';
-import './node_modules/@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.min.js';
+import * as L from './node_modules/leaflet/dist/leaflet-src.esm.js';
+
+// Uncomment if not lazy loading geoman
+// import './node_modules/@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.min.js';
 
 // We’ll add a tile layer to add to our map, in this case it’s a OSM tile layer.
 	 	// Creating a tile layer usually involves setting the URL template for the tile images
@@ -25,6 +27,9 @@ import './node_modules/@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.min.js
         editPolygon: true,  // adds button to toggle global edit mode
         deleteLayer: true   // adds a button to delete layers
     };
+
+    await import('./node_modules/@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.min.js');
+    L.PM.reInitLayer(map)
 
     // add leaflet.pm controls to the map
     map.pm.addControls(options);
